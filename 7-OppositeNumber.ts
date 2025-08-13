0053b1a3297c1775b62b0ee90e3da25687e1d16a
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-13 23:39:09 +0300

    problem 7

---
+++ 7-OppositeNumber.ts
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+// This is My Solution
+
+export class Kata {
+  static opposite(n: number) {
+    return -n;
+  }
+}
+
+console.log(Kata.opposite(14))
