2ea177017fbdf101f8d518a960010c6386d07daf
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-13 23:23:33 +0300

    problem 5

---
+++ 5-ConvertNumberToReversedArrayOfDigits.ts
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5583090cbe83f4fd8c000051
+// This is My Solution
+
+export const digitize = (n: number): number[] => {
+  const solution: number[] = Array.from(n.toString())
+    .reverse()
+    .map((elem) => +elem);
+  return solution;
+};
