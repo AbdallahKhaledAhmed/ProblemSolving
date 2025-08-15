36618370ebb3c5271da5bd2128737439b60e0647
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-15 23:20:25 +0300

    problem 22

---
+++ CodeWars/8 kyu/22-CalculateAverage.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57a2013acf1fa5bfc4000921
+// This is My Solution
+
+export function findAverage(array: number[]): number {
+  return array.length === 0
+    ? 0
+    : array.reduce((total, current) => (total += current), 0) / array.length;
+}
