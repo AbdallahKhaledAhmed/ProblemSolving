b6464fd44a76114b6e14f257a781b767c8adb5c5
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-15 22:55:56 +0300

    problem 19

---
+++ CodeWars/8 kyu/19-ReversedWords.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/51c8991dee245d7ddf00000e
+// This is My Solution
+
+export function reverseWords(str: string) {
+  return str.split(" ").reverse().join(" ");
+}
+
+console.log(reverseWords("Hello World"));
