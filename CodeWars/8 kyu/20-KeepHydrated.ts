0208475837badf023002ef5bd672bdee43ee791c
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-15 22:57:40 +0300

    problem 20

---
+++ CodeWars/8 kyu/20-KeepHydrated.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/582cb0224e56e068d800003c
+// This is My Solution
+
+export function litres(time: number): number {
+  return Math.floor(time / 2);
+}
+
+console.log(litres(11.8));
