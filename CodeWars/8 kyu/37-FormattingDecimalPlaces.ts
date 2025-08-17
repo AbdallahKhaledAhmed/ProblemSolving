1a767b4a938353cc5c931ced2f3ac3339448fca7
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-17 19:04:02 +0300

    problem 37

---
+++ CodeWars/8 kyu/37-FormattingDecimalPlaces.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5641a03210e973055a00000d
+// This is My Solution
+
+function twoDecimalPlaces(n: number): number {
+  return Number(n.toFixed(2));
+}
+
+console.log(twoDecimalPlaces(1234.12345));
