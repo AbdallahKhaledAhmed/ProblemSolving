fa09449abfd1bbcbe28a38ff2765525bc0b9f5eb
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-17 18:44:02 +0300

    problem 36

---
+++ CodeWars/8 kyu/36- VolumeOfACuboid.ts
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a
+// This is My Solution
+
+export function getVolumeOfCuboid(
+  length: number,
+  width: number,
+  height: number
+): number {
+  return length * width * height;
+}
+
+console.log(getVolumeOfCuboid(10, 10, 10));
