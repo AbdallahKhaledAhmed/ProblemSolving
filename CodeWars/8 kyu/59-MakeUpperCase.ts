8ddd70c08fb794afaa715e919138cc01e665bb7e
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-19 00:37:22 +0300

    problem 59

---
+++ CodeWars/8 kyu/59-MakeUpperCase.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+// This is My Solution
+
+export function makeUpperCase(str: string): string {
+  return str.toUpperCase();
+}
+
+console.log(makeUpperCase("Hello World"));
