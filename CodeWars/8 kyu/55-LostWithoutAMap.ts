b3b1f5484f4c31ba52396acf82d0186cb928cf93
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-19 00:11:09 +0300

    problem 55

---
+++ CodeWars/8 kyu/55-LostWithoutAMap.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e
+// This is My Solution
+
+export function maps(x: number[]): number[] {
+  return x.map((ele) => ele * 2);
+}
+
+console.log(maps([1, 2, 3, 4]));
