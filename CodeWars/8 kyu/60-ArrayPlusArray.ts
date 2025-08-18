b5d50b48484c014055b6fcd2d50a6416c48bd89f
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-19 00:47:34 +0300

    problem 60

---
+++ CodeWars/8 kyu/60-ArrayPlusArray.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+// This is My Solution
+
+export const arrayPlusArray = (arr1: number[], arr2: number[]): number => {
+  return [...arr1, ...arr2].reduce((sum, current) => (sum += current), 0); // something went wrong ?
+};
+
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]));
