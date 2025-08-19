9c1f6d55e42ddabeedeb0b7e2793493e7c1a12cc
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-19 19:20:42 +0300

    problem 67

---
+++ CodeWars/8 kyu/67-HowManyLightsabersDoYouOwn.ts
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/51f9d93b4095e0a7200001b8
+// This is My Solution
+
+export function howManyLightsabersDoYouOwn(name?: any): number {
+  return name === "Zach" ? 18 : 0;
+}
+
+console.log(howManyLightsabersDoYouOwn("Zach"))
+console.log(howManyLightsabersDoYouOwn("Abdo"))
