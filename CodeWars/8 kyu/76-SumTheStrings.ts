eb2c7ebbdb23e68bca62d3c9ba9d84182b62d9ae
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-20 22:31:35 +0300

    problem 76

---
+++ CodeWars/8 kyu/76-SumTheStrings.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5966e33c4e686b508700002d
+// This is My Solution
+
+export function sumStr(a: string, b: string): string {
+  return `${+a + +b}`;
+}
+
+console.log(sumStr("6", "7"));
