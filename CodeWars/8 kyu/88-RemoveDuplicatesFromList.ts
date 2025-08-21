bbb4ebd0520662d26419fda9a48f5599664cf0ee
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-22 02:11:23 +0300

    problem 88

---
+++ CodeWars/8 kyu/88-RemoveDuplicatesFromList.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118
+// This is My Solution
+
+export const distinct = (a: number[]): number[] => {
+  return a.filter((ele, ind) => a.indexOf(ele) === ind);
+};
+
+console.log(distinct([1, 1, 2]));
