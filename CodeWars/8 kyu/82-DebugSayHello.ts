75879ec3b17a59c77eacd4a133869b3da5cbce3d
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-22 00:50:35 +0300

    problem 82

---
+++ CodeWars/8 kyu/82-DebugSayHello.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5625618b1fe21ab49f00001f
+// This is My Solution
+
+export function sayHello(name: string): string {
+  return "Hello, " + name;
+}
+
+console.log(sayHello("Abdo"))
