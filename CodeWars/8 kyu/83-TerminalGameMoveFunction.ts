4fdd942d1945e3fe2697407c0f332e1c0760fed0
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-22 00:55:34 +0300

    problem 83

---
+++ CodeWars/8 kyu/83-TerminalGameMoveFunction.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/563a631f7cbbc236cf0000c2
+// This is My Solution
+
+export function move(pos: number, roll: number): number {
+  return pos + roll * 2;
+}
+
+console.log(move(3, 6));
