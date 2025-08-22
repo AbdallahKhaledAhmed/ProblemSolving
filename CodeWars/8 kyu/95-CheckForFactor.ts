8dff3bc3b0a9855ae4db8229c23ec0685d956557
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-08-22 21:04:54 +0300

    problem 95

---
+++ CodeWars/8 kyu/95-CheckForFactor.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/55cbc3586671f6aa070000fb
+// This is My Solution
+
+export function checkForFactor(base: number, factor: number) {
+  return base % factor === 0;
+}
+
+console.log(checkForFactor(6, 2));
