e4d8328dc15ae96145c5bcac57e57fdb74e3aa70
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-09-02 01:30:13 +0300

    problem 11 kuy 7 other sol

--- CodeWars/7 kyu/11-ShortestWord.ts
+++ CodeWars/7 kyu/11-ShortestWord.ts
@@ -11,4 +11,11 @@ export function findShort(s: string): number {
     );
 }
 
+// Other Good Solution (Not Mine)
+
+export function findShort1(s: string): number {
+  return Math.min(...s.split(" ").map((ele) => ele.length));
+}
+
 console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
+console.log(findShort1("bitcoin take over the world maybe who knows perhaps"));
