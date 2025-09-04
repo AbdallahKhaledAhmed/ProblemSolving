2629e87c6b342f0ccc3b092aed6e6a3e92fcf929
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-09-05 00:41:54 +0300

    problem 9 kyu 6

---
+++ CodeWars/6 kyu/9-FindTheParityOutlier.ts
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5526fc09a1bbd946250002dc
+// This is My Solution
+
+export function findOutlier(integers: number[]): number {
+  let evens: number[] = [];
+  let odds: number[] = [];
+  integers.forEach((ele) => (ele % 2 === 0 ? evens.push(ele) : odds.push(ele)));
+  return evens.length === 1 ? evens[0] : odds[0];
+}
