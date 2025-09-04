f379aa5b796dcebe8c86b1fdd794d053c670f830
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-09-05 02:11:45 +0300

    problem 14 kyu 6

---
+++ CodeWars/6 kyu/14-UniqueInOrder.ts
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/54e6533c92449cc251001667
+// This is My Solution
+
+export function uniqueInOrder(
+  iterable: string | (string | number)[]
+): (string | number)[] {
+  return typeof iterable === "string"
+    ? iterable.split("").filter((ele, ind, arr) => ele !== arr[ind - 1])
+    : iterable.filter((ele, ind, arr) => ele !== arr[ind - 1]);
+}
+
+console.log(uniqueInOrder("AAAABBBCCDAABBB"));
+console.log(uniqueInOrder("ABBCcAD"));
+console.log(uniqueInOrder([1, 2, 2, 3, 3]));
