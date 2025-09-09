14c1d3d842d48d7abb8d09ceba3ff2d500df710f
Abdallah Khaled <abdallahkhaled9321@gmail.com>
2025-09-10 01:27:42 +0300

    finally !

--- CodeWars/6 kyu/1-TheLatestClock.ts
+++ CodeWars/6 kyu/1-TheLatestClock.ts
@@ -2,39 +2,28 @@
 // This is My Solution
 
 export function latestClock(...args: number[]) {
-  args.sort().reverse();
-  const hours: number[][][] = [];
-  const mins: number[][][] = [];
-  args.forEach((num, ind, arr) =>
-    arr.forEach((num1, ind1) => {
-      if (ind !== ind1) {
-        const mix = Number(num.toString() + num1);
-        if (mix < 24 )
-          hours.push([
-            [num, num1],
-            [ind, ind1],
-          ]);
-        else if (mix < 60 )
-          mins.push([
-            [num, num1],
-            [ind, ind1],
-          ]);
+  const time: string[] = [];
+  args.forEach((e1, i1) => {
+    args.forEach((e2, i2) => {
+      if (i2 !== i1) {
+        args.forEach((e3, i3) => {
+          if (i3 !== i2 && i3 !== i1) {
+            args.forEach((e4, i4) => {
+              if (i4 !== i3 && i4 !== i2 && i4 !== i1) {
+                time.push(e1.toString() + e2 + e3 + e4);
+              }
+            });
+          }
+        });
       }
-    })
-  );
-  for (let i = 0; i < hours.length; i++) {
-    for (let ind = 0; ind < mins.length; ind++) {
-      if (
-        !(
-          mins[ind][1].includes(hours[i][1][0]) ||
-          mins[ind][1].includes(hours[i][1][1])
-        )
-      ) {
-        return `${hours[i][0].join("")}:${mins[ind][0].join("")}`;
-      }
-    }
-  }
-  return `00:00`;
+    });
+  });
+  const lastTime = time
+    .sort()
+    .reverse()
+    .filter((ele) => +ele.slice(0, 2) < 24 && +ele.slice(2) < 60)[0];
+
+  return lastTime.slice(0, 2) + ":" + lastTime.slice(2);
 }
 
 console.log(latestClock(9, 1, 2, 5));
